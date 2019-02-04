4baa57b308d40c58fabb980f31c9bfab9342e46b
WataruSuzuki <wataru0406@gmail.com>
2019-02-04 22:40:00 +0900

    Moving first sign in sequence from main activity to service

--- feature/src/main/java/dev/jchankchan/mealdock/feature/FirebaseService.kt
+++ feature/src/main/java/dev/jchankchan/mealdock/feature/FirebaseService.kt
@@ -1,9 +1,14 @@
 package dev.jchankchan.mealdock.feature
 
+import android.app.Activity
 import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class FirebaseService : Service() {
 
@@ -18,5 +23,45 @@ class FirebaseService : Service() {
             get() = this@FirebaseService
     }
 
+    val RC_SIGN_IN = 9999
+    fun requestToAuth(activity: Activity) {
+        // Choose authentication providers
+        val providers = arrayListOf(
+                //AuthUI.IdpConfig.GoogleBuilder().build(),
+                //AuthUI.IdpConfig.PhoneBuilder().build(),
+                //AuthUI.IdpConfig.TwitterBuilder().build(),
+                //AuthUI.IdpConfig.FacebookBuilder().build(),
+                AuthUI.IdpConfig.EmailBuilder().build())
+        // Create and launch sign-in intent
+
+        activity.startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN)
+    }
+
+    fun onResultAuth(resultCode: Int, data: Intent?) {
+        val response = IdpResponse.fromResultIntent(data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            // Successfully signed in
+            currentUser = FirebaseAuth.getInstance().currentUser
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
+
+    private var currentUser : FirebaseUser? = null
+    fun startSession(activity: Activity) {
+        if (currentUser == null) {
+            requestToAuth(activity)
+        }
+    }
 
 }
--- feature/src/main/java/dev/jchankchan/mealdock/feature/MainActivity.kt
+++ feature/src/main/java/dev/jchankchan/mealdock/feature/MainActivity.kt
@@ -1,13 +1,13 @@
 package dev.jchankchan.mealdock.feature
 
-import android.app.Activity
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
-import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -35,43 +35,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        val intent = Intent(this, FirebaseService::class.java)
+        bindService(intent, firebaseServiceConnection, Context.BIND_AUTO_CREATE)
     }
 
-    override fun onStart() {
-        super.onStart()
-
-        // Choose authentication providers
-        val providers = arrayListOf(
-//                AuthUI.IdpConfig.GoogleBuilder().build(),
-//                AuthUI.IdpConfig.PhoneBuilder().build(),
-//                AuthUI.IdpConfig.TwitterBuilder().build(),
-//                AuthUI.IdpConfig.FacebookBuilder().build(),
-                AuthUI.IdpConfig.EmailBuilder().build())
-        // Create and launch sign-in intent
-
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .build(),
-                RC_SIGN_IN)
-    }
-
-    private val RC_SIGN_IN = 9999
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        val response = IdpResponse.fromResultIntent(data)
-
-        if (resultCode == Activity.RESULT_OK) {
-            // Successfully signed in
-            val user = FirebaseAuth.getInstance().currentUser
-            // ...
-        } else {
-            // Sign in failed. If response is null the user canceled the
-            // sign-in flow using the back button. Otherwise check
-            // response.getError().getErrorCode() and handle the error.
-            // ...
+        when (requestCode) {
+            firebaseService.RC_SIGN_IN -> firebaseService.onResultAuth(resultCode, data)
         }
     }
+
+    private lateinit var firebaseService : FirebaseService
+    private val firebaseServiceConnection = object : ServiceConnection {
+
+        override fun onServiceConnected(className: ComponentName,
+                               binder: IBinder) {
+            val bindService = binder as FirebaseService.FirebaseApiBinder
+            firebaseService = bindService.service
+            firebaseService.startSession(this@MainActivity)
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {}
+    }
+
 }
