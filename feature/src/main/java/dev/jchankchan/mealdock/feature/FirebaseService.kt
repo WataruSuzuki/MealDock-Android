caee621ec6cfaabd7be989b4f05b859b162847f1
WataruSuzuki <wataru0406@gmail.com>
2019-02-05 22:10:00 +0900

    Added silent sign in function via email and password

--- feature/src/main/java/dev/jchankchan/mealdock/feature/FirebaseService.kt
+++ feature/src/main/java/dev/jchankchan/mealdock/feature/FirebaseService.kt
@@ -5,6 +5,8 @@ import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import android.util.Log
+import android.widget.Toast
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
@@ -13,6 +15,12 @@ import com.google.firebase.auth.FirebaseUser
 class FirebaseService : Service() {
 
     private val firebaseApiBinder = FirebaseApiBinder()
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate() {
+        super.onCreate()
+        auth = FirebaseAuth.getInstance()
+    }
 
     override fun onBind(intent: Intent): IBinder? {
         return firebaseApiBinder
@@ -23,6 +31,7 @@ class FirebaseService : Service() {
             get() = this@FirebaseService
     }
 
+    val TAG = FirebaseService::class.java.simpleName
     val RC_SIGN_IN = 9999
     fun requestToAuth(activity: Activity) {
         // Choose authentication providers
@@ -60,8 +69,43 @@ class FirebaseService : Service() {
     private var currentUser : FirebaseUser? = null
     fun startSession(activity: Activity) {
         if (currentUser == null) {
-            requestToAuth(activity)
+            signInWith(activity, "member_unittest@mealdock.com", "unittest")
         }
     }
 
+    fun signInWith(activity: Activity, email: String, password: String) {
+
+        //var config = AuthUI.IdpConfig
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(activity) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        currentUser = auth.currentUser
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                        requestToAuth(activity)
+                    }
+                }
+
+    }
+
+    fun signOut() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener {
+                    // ...
+                }
+    }
+
+    fun deleteAccount() {
+        AuthUI.getInstance()
+                .delete(this)
+                .addOnCompleteListener {
+                    // ...
+                }
+    }
 }
